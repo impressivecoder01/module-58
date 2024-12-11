bba63a18a6a4dee9d0adb14d67f37fc77d025fa0
impressivecoder01 <rohanrahman251@gmail.com>
2024-12-11 22:27:20 +0600

    banner animation

--- src/Pages/Banner/Banner.jsx
+++ src/Pages/Banner/Banner.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import {easeOut, motion} from 'framer-motion'
+import team1 from '../../assets/team1/download (2).png'
 const Banner = () => {
   return (
     <div className="hero bg-base-200 min-h-96">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="flex-1">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-            className="max-w-sm rounded-lg shadow-2xl"
+          <motion.img
+            src={team1}
+            animate={{y: [0, 50, 0]}}
+            transition={{duration:10, repeat: Infinity}}
+            
+            className="max-w-sm w-64 rounded-t-3xl rounded-br-3xl border-l-4 border-b-4 shadow-2xl"
           />
         </div>
 
@@ -15,7 +19,7 @@ const Banner = () => {
           <motion.h1 
           animate={{x: 50,}}
           transition={{duration:2, delay: 1, ease:easeOut, repeat: Infinity}}
-          className="text-5xl font-bold">Job News !</motion.h1>
+          className="text-5xl font-bold">Latest <motion.span animate={{color:['#ecff33', '#33ffe3', ]}} transition={{duration:5 , repeat:Infinity}}>Job</motion.span> News !</motion.h1>
           <p className="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
